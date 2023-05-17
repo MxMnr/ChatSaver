8c4d697921cb4aa24e729e4c675ca88ff3607a7f
MxMnr <maxwell.miner@gmail.com>
2023-05-17 17:19:00 +0000

    Updating output format and file naming

--- content.js
+++ content.js
@@ -11,8 +11,11 @@ chrome.runtime.onMessage.addListener(
       let file = new Blob([textData], {type: 'text/plain'});
       let fileURL = URL.createObjectURL(file);
 
+      // Prepare the file name by replacing spaces with underscores and removing any special characters
+      let fileName = 'chatSaver_' + document.title.replace(/[^a-z0-9]/gi, '_') + '.txt';
+
       let windowObjectReference = window.open();
-      windowObjectReference.document.write('<html><body><a href="'+fileURL+'" download="chat.txt">Download Chat</a></body></html>');
+      windowObjectReference.document.write('<html><body><a href="'+fileURL+'" download="'+fileName+'">Download Chat</a></body></html>');
     }
   }
 );
